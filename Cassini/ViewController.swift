2a57fdc52eb70a8730d0714ebfcac5c773b9a169
jeonghwan <ej88ej@gmail.com>
2016-03-19 15:24:25 +0800

    Add additional view for loading iamge

---
+++ Cassini/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by Chris on 2016. 3. 19..
+//  Copyright © 2016년 WePlanet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+}
+
